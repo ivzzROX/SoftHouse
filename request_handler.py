29041cc1f71802a5c2f092406fff2cd81441b0f6
vyshnivskyi <daniil.vyshnivskyi@nixsolutions.com>
2020-10-11 22:10:12 +0300

    Add back for user logic. Problem with indexing

--- request_handler.py
+++ request_handler.py
@@ -12,7 +12,6 @@
 # sensor with address fffe always equal 0
 # sensor with address ffff always equal 1
 
-LOGIC = {"START": 1, "OR": 2, "AND": 3, "XOR": 4, "NOR": 5, "NAND": 6, "XNOR": 7, "NOT": 8}
 
 import time
 from flask import jsonify, request, Response
@@ -21,6 +20,8 @@ from flask_restful import Resource
 from config import sensor_type
 from data_providers.LogicNode import LogicNode
 from db import Sensors, Devices
+
+LOGIC = {"START": 1, "OR": 2, "AND": 3, "XOR": 4, "NOR": 5, "NAND": 6, "XNOR": 7, "NOT": 8}
 
 
 class TestEndpoint(Resource):
@@ -164,49 +165,69 @@ class RegisterDevice(Resource):
         Devices.create(serial_number=serial_number)
 
 
-def get_longest_branch(node, counter):
-    if node.node_type == 'OUT':
+def get_longest_branch(node, counter=0):
+    if node.node_type == 'OUTPUT':
         return counter
     else:
-        get_longest_branch(node.link_to, counter + 1)
+        return get_longest_branch(node.link_to, counter + 1)
 
 
 def check_if_in_longest_branch(node, current_node=None):
     if not current_node.link_to:
-        raise Exception('Node not found')
-    if current_node == node:
+        return False
+    if current_node.node_id == node.node_id:
         return True
     else:
-        check_if_in_longest_branch(current_node.link_to, node)
+        return check_if_in_longest_branch(node, current_node.link_to)
 
 
 def form_branch(node, branches, current_branch_id, start_node_for_longest_branch=None):
     if node.node_type == 'OUTPUT':
-        return form_branch(node.link_from1, branches, current_branch_id=1)
+        branches.update({0: []})
+        return form_branch(node.link_from1, branches, current_branch_id=0,  # create initial branch
+                           start_node_for_longest_branch=start_node_for_longest_branch)
     if node.link_from1.node_type == 'INPUT' and node.link_from2.node_type == 'INPUT':
-        branches.get(current_branch_id).insert(0, f'{{{node.link_from1.data}: 1: {node.link_from1.data}}}')
-        branches.get(current_branch_id).insert(0, f'{{{node.link_from1.data}: 1: {node.link_from1.data}}}')
+        branches.get(current_branch_id).insert(0,
+                                               # .insert(0,foo) to insert at the begging of the list due to step-by-step
+                                               f'{{{node.link_from2.data}: {LOGIC.get(node.logic_type)}: {node.link_from2.value}}}')  # add second item
+        branches.get(current_branch_id).insert(0,
+                                               f'{{{node.link_from1.data}: 1: {node.link_from1.value}}}')  # add start
         return branches
     if (node.link_from1.node_type == 'INPUT' and node.link_from2.node_type == 'LOGIC') or (
-            node.link_from2.node_type == 'INPUT' and node.link_from1.node_type == 'LOGIC'):
+            node.link_from2.node_type == 'INPUT' and node.link_from1.node_type == 'LOGIC'):  # TODO refactor for smaller if
+        # insert into branch input value and go next logic node
         if node.link_from1.node_type == 'INPUT':
             branches.get(current_branch_id).insert(0,
-                                                   f'{{{node.link_from1.data}: {node.logic_type}: {node.link_from1.data}}}')
-            return form_branch(node.link_from2, branches, current_branch_id)
+                                                   f'{{{node.link_from1.data}: {LOGIC.get(node.logic_type)}: {node.link_from1.value}}}')
+            return form_branch(node.link_from2, branches, current_branch_id,
+                               start_node_for_longest_branch=start_node_for_longest_branch)
         else:
             branches.get(current_branch_id).insert(0,
-                                                   f'{{{node.link_from2.data}: {node.logic_type}: {node.link_from2.data}}}')
-            return form_branch(node.link_from1, branches, current_branch_id)
+                                                   f'{{{node.link_from2.data}: {LOGIC.get(node.logic_type)}: {node.link_from2.value}}}')
+            return form_branch(node.link_from1, branches, current_branch_id,
+                               start_node_for_longest_branch=start_node_for_longest_branch)
     if node.link_from1.node_type == 'LOGIC' and node.link_from2.node_type == 'LOGIC':
-        if check_if_in_longest_branch(node.link_from1, start_node_for_longest_branch):
-            form_branch(node.link_from2, branches, current_branch_id)
-        form_branch(node.link_from1, branches, current_branch_id + 1)
+        branches.get(current_branch_id).insert(0,
+                                               f'{{s{current_branch_id + 1}: {LOGIC.get(node.logic_type)}}}')  # add branch item to list
+        branches.update({len(branches.values()): []})  # create new branch
+        if check_if_in_longest_branch(node.link_from1,
+                                      start_node_for_longest_branch):  # if in longest branch create new branch for another node and insert current
+            form_branch(node.link_from1, branches, current_branch_id,
+                        start_node_for_longest_branch=start_node_for_longest_branch)
+            form_branch(node.link_from2, branches, current_branch_id + 1,
+                        start_node_for_longest_branch=start_node_for_longest_branch)
+        else:
+            form_branch(node.link_from2, branches, current_branch_id,
+                        start_node_for_longest_branch=start_node_for_longest_branch)
+            form_branch(node.link_from1, branches, current_branch_id + 1,
+                        start_node_for_longest_branch=start_node_for_longest_branch)
 
 
 def get_branches(nodes, start_node_for_longest_branch):
     out = nodes.get('out')
     branches = {}
-    form_branch(out, branches, 1)
+    form_branch(out, branches, 0, start_node_for_longest_branch)
+    return branches
 
 
 class GetUserLogic(Resource):
@@ -220,22 +241,37 @@ class GetUserLogic(Resource):
         for _object in objects:
             node = LogicNode(node_id=_object.get('id'), node_type=_object.get('type'),
                              logic_type=_object.get('logicType'),
-                             data=_object.get('t'))
+                             data=_object.get('t'), value=_object.get('v'))
             nodes.append(node)
 
             nodes_id_dict.update({node.node_id: node})
             if node.node_type == 'OUTPUT':
                 nodes_id_dict.update({'out': node})
-        print(links)
+        del objects
+        del _object
         for node in nodes:
             # if node.node_type == 'LOGIC':
+            print('1')
             for link in links:
                 if node.node_id == link.get('to').get('id'):
+                    if node.node_type == 'OUTPUT':
+                        node.link_from1 = nodes_id_dict.get(link.get('from').get('id'))
+                        break
                     node_link = nodes_id_dict.get(link.get('from').get('id'))
                     if not node.link_from1:
                         node.link_from1 = node_link
                     else:
                         node.link_from2 = node_link
+
                 elif node.node_id == link.get('from').get('id'):
-                    node.link_to = nodes_id_dict.get(link.get('from').get('id'))
+                    node.link_to = nodes_id_dict.get(link.get('to').get('id'))
+        start_node_for_longest_branch = None
+        counter = 0
+        for node in nodes:
+            if node.node_type == 'INPUT':
+                current_counter = get_longest_branch(node)
+                if current_counter > counter:
+                    counter = current_counter
+                    start_node_for_longest_branch = node
             print(node.print_val() + '\n')
+        print(get_branches(nodes_id_dict, start_node_for_longest_branch))
